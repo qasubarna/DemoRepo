448ea6ddcdf27d7cfc4f80ff11c527efd13b3532
subarnak <subarnak2@gmail.com>
2020-09-16 14:07:45 -0400

    Changed name

--- src/main/java/org/example/gitHubDemo/IntJGitHub.java
+++ src/main/java/org/example/gitHubDemo/IntJGitHub.java
@@ -3,6 +3,6 @@ package org.example.gitHubDemo;
 public class IntJGitHub {
     public static void main(String[] args) {
         System.out.println("Hello Git");
-        System.out.println("qaDemo");
+        System.out.println("Subarna");
     }
 }
