ff9f65071f3f4e2116311d7b6b68bbbc39228a32
subarnak <subarnak2@gmail.com>
2020-09-16 13:51:47 -0400

    first git commit

---
+++ src/main/java/org/example/gitHubDemo/IntJGitHub.java
@@ -0,0 +1,8 @@
+package org.example.gitHubDemo;
+
+public class IntJGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+        System.out.println("qaDemo");
+    }
+}
